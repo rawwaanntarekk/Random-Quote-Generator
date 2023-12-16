b8d4affe7c4989990024c4e68652088755159a3f
Rawan Tarek <rawan25112003@gmail.com>
2023-12-16 16:41:04 +0200

    changing the directory of images to be visible in github pages

--- js/app.js
+++ js/app.js
@@ -17,51 +17,49 @@ var quotes = [
   {
     quote: "“Be yourself; everyone else is already taken.”",
     author: "― Oscar Wilde",
-    image: `<img src="../Images/1.jpg" class="rounded-circle w-100  ">`,
+    image: `<img src="./Images/1.jpg" class="rounded-circle w-100  ">`,
   },
   {
     quote:
       "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
     author: "― Albert Einstein",
-    image: `<img src="../Images/2.jpg" class="rounded-circle w-100 ">`,
+    image: `<img src="./Images/2.jpg" class="rounded-circle w-100 ">`,
   },
   {
     quote: "“A room without books is like a body without a soul.”",
     author: "― Marcus Tullius Cicero",
-    image: `<img src="../Images/3.jpg" class="rounded-circle w-100 ">`,
+    image: `<img src="./Images/3.jpg" class="rounded-circle w-100 ">`,
   },
   {
     quote: "“Be the change that you wish to see in the world.”",
     author: "― Mahatma Gandhi",
-    image: `<img src="../Images/4.jpg" class="rounded-circle w-100 ">`,
+    image: `<img src="./Images/4.jpg" class="rounded-circle w-100 ">`,
   },
   {
     quote:
       "“Live as if you were to die tomorrow. Learn as if you were to live forever.”",
     author: "― Mahatma Gandhi",
-    image: `<img src="../Images/4.jpg" class="rounded-circle w-100 ">`,
+    image: `<img src="./Images/4.jpg" class="rounded-circle w-100 ">`,
   },
   {
     quote:
       "“I am so clever that sometimes I don't understand a single word of what I am saying.”",
     author: "― Oscar Wilde, The Happy Prince and Other Stories",
-    image: `<img src="../Images/1.jpg" class="rounded-circle w-100 ">`,
+    image: `<img src="./Images/1.jpg" class="rounded-circle w-100 ">`,
   },
   {
     quote:
       "“To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.”",
     author: "― Ralph Waldo Emerson",
-    image: `<img src="../Images/5.jpg" class="rounded-circle w-100 ">`,
+    image: `<img src="./Images/5.jpg" class="rounded-circle w-100 ">`,
   },
   {
     quote:
       "“It is better to be hated for what you are than to be loved for what you are not.”",
     author: "― Andre Gide, Autumn Leaves",
-    image: `<img src="../Images/6.jpg" class="rounded-circle w-100 ">`,
+    image: `<img src="./Images/6.jpg" class="rounded-circle w-100 ">`,
   },
 ];
-
-// localStorage.setItem("quotes", JSON.stringify(quotes));
 
 //^ Functions
 
